0862cc97c1b2318cde21e0037c016369947f6138
ritiksha <ritu.patel597@gmail.com>
2022-01-01 15:48:55 -0500

    hello

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,17 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { PricingComponent } from './pricing/pricing.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: 'home', component: HomeComponent },
+  { path: 'pricing', component: PricingComponent },
   { path: '**', redirectTo: 'home' },
 ];
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, HeaderComponent, FooterComponent],
+  declarations: [AppComponent, HomeComponent, HeaderComponent, FooterComponent, PricingComponent],
   imports: [BrowserModule, AppRoutingModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent],
